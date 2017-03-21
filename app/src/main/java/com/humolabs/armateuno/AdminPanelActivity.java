c4267ce17e089714f9e9325135207fa46acc4505
humolabs <humolabs@gmail.com>
2017-03-21 00:29:24 -0300

    varios cambios

--- app/src/main/java/com/humolabs/armateuno/AdminPanelActivity.java
+++ app/src/main/java/com/humolabs/armateuno/AdminPanelActivity.java
@@ -1,16 +1,63 @@
 package com.humolabs.armateuno;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.github.jjobes.slidedatetimepicker.SlideDateTimeListener;
+import com.github.jjobes.slidedatetimepicker.SlideDateTimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by Christus on 5/3/2017.
  */
-public class AdminPanelActivity extends Activity {
+public class AdminPanelActivity extends FragmentActivity {
+
+    EditText btnFechaHora;
+    private SimpleDateFormat mFormatter = new SimpleDateFormat("MMMM dd yyyy hh:mm aa");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.admin_panel_activity);
         super.onCreate(savedInstanceState);
+
+
+        btnFechaHora = (EditText) findViewById(R.id.btnfechahora);
+        btnFechaHora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new SlideDateTimePicker.Builder(getSupportFragmentManager())
+                        .setListener(listener)
+                        .setInitialDate(new Date())
+                        //.setMinDate(minDate)
+                        //.setMaxDate(maxDate)
+                        //.setIs24HourTime(true)
+                        //.setTheme(SlideDateTimePicker.HOLO_DARK)
+                        //.setIndicatorColor(Color.parseColor("#990000"))
+                        .build()
+                        .show();
+            }
+        });
     }
+
+    private SlideDateTimeListener listener = new SlideDateTimeListener() {
+
+        @Override
+        public void onDateTimeSet(Date date)
+        {
+            Toast.makeText(AdminPanelActivity.this, mFormatter.format(date), Toast.LENGTH_SHORT).show();
+            btnFechaHora.setText(mFormatter.format(date));
+        }
+
+        // Optional cancel listener
+        @Override
+        public void onDateTimeCancel()
+        {
+            Toast.makeText(AdminPanelActivity.this, "Canceled", Toast.LENGTH_SHORT).show();
+        }
+    };
 }
--- app/src/main/java/com/humolabs/armateuno/LandingActivity.java
+++ app/src/main/java/com/humolabs/armateuno/LandingActivity.java
@@ -1,24 +1,30 @@
 package com.humolabs.armateuno;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
+
 
 /**
  * Created by Christus on 5/3/2017.
  */
 
-public class LandingActivity extends Activity {
+public class LandingActivity extends AppCompatActivity {
 
     TextView btnProxPartidos;
     TextView btnPanelAdmin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setContentView(R.layout.landing_activity);
+
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing_activity);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
 
         btnProxPartidos = (TextView) findViewById(R.id.btnProxPartidos);
         btnPanelAdmin = (TextView) findViewById(R.id.btnPanelAdmin);
